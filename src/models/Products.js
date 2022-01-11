bf10ffaa0083acae731b00b1a564f3e6d3159842
SalvaBandicoot <salvabandicoot@gmail.com>
2022-01-11 10:49:31 +0100

    Add pagination

--- src/models/Products.js
+++ src/models/Products.js
@@ -1,22 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const pagination = require("mongoose-paginate-v2");
+
 const productSchema = new Schema({
-  id: {type: mongoose.Schema.ObjectId},
-  name: { type: String, lowercase: true , required: true},
-  description : {type: String},
-  price: {type: Number, required: true},
-  stock: {type: Number, required: true},
-  image: [imageSchema] 
+  id: { type: mongoose.Schema.ObjectId },
+  name: { type: String, lowercase: true, required: true },
+  description: { type: String },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  image: [imageSchema],
 });
 
 const imageSchema = new Schema({
-  title: {type: String},
-  url: {type: String},
-  alt: {type: String}
-})
+  title: { type: String },
+  url: { type: String },
+  alt: { type: String },
+});
+
+productSchema.plugin(pagination);
 
 module.exports = {
   productSchema,
-  imageSchema
-}
+  imageSchema,
+};
